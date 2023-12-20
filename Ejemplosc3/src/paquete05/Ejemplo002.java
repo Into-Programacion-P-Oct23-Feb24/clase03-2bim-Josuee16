c7754365c6eec99f66e1ee8e1b3edce72a8d6aa4
Josuee16 <josuexpardoch@gmail.com>
2023-12-20 16:20:50 -0500

    Ejemplo002

--- Ejemplosc3/src/paquete05/Ejemplo002.java
+++ Ejemplosc3/src/paquete05/Ejemplo002.java
@@ -15,8 +15,13 @@ public class Ejemplo002 {
         int[][] arreglo = {{10, 41, 40}, {1, 2, 3}, {1, 12, 4}};
         for (int f = 0; f < arreglo.length; f++) {
             for (int c = 0; c < arreglo[f].length; c++) {
-                if (f==c){
+                if (f==c){ 
                     suma = suma + arreglo [f][c];
+                    /*
+                    Se crea una condicion donde el numero de filas tiene que ser
+                    igual al numero de columnas, ya que aqui estan ubicados los 
+                    valores requeriods y solo se crea una variable suma
+                    */
                 }
             }
         }

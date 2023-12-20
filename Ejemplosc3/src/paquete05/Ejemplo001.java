f2904e46febc4373dc5713aebe09bab2f3a9f02d
Josuee16 <josuexpardoch@gmail.com>
2023-12-20 15:35:57 -0500

    Arreglo unidimensional

--- Ejemplosc3/src/paquete05/Ejemplo001.java
+++ Ejemplosc3/src/paquete05/Ejemplo001.java
@@ -8,14 +8,13 @@ package paquete05;
  *
  * @author LAB.ELECT
  */
-public class Ejemplo {
+public class Ejemplo001 {
 
     public static void main(String[] args) {
         int[] arreglo = new int[2];
         for (int i = 0; i < arreglo.length; i++) {
-            
+
             System.out.printf("%s", arreglo[i]);
         }
-
     }
 }
